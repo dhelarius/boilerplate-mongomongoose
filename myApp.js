9d112c254ac86859f4e8bc007fc611644b66d3cb
dhelarius <jeffrey.ayala2040@gmail.com>
2021-10-22 11:47:12 -0400

    Configure project

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 let Person;
 
