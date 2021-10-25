a6644287c99def11504b4248d443fd6fdb7bf562
dhelarius <jeffrey.ayala2040@gmail.com>
2021-10-25 08:38:00 -0400

    Add comment #1

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const MONGO_URI = process.env.MONGO_URI;
 
+//* 1- Establecer conexión con MongoDB Atlas */
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 let Person;
